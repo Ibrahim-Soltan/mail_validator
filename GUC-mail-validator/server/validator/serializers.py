a9f0d8ad480dc6a4f210f545264e89450de0cbd3
Ibrahim Soltan <ibrahim.soltan918@gmail.com>
2022-10-10 02:45:26 +0200

    docs: Adding comments to code.

--- GUC-mail-validator/server/validator/serializers.py
+++ GUC-mail-validator/server/validator/serializers.py
@@ -3,14 +3,18 @@ from rest_framework import serializers
 
 
 def is_matching(mail):
+    # Check if the mail passed matches the Regex of a GUC mail.
     match = re.match("^[a-z]+[.][a-z]+@student[.]guc[.]edu[.]eg$", mail)
+    # Return true if the mail matches the pattern and is composed < 254 characters.
     return (len(mail) <= 254) and bool(match)
 
 
 def is_guc_mail(value):
+    # If the mail is not valid raise error with adequate message.
     if not is_matching(value):
         raise serializers.ValidationError('Invalid GUCmail')
 
 
 class MailSerializer(serializers.Serializer):
+    # Apply validation to the mail attribute.
     mail = serializers.CharField(validators=[is_guc_mail])
--- GUC-mail-validator/server/validator/views.py
+++ GUC-mail-validator/server/validator/views.py
@@ -6,8 +6,11 @@ from .serializers import MailSerializer
 
 
 class Validator(APIView):
+    # Post Endpoint:
     def post(self, request):
+        # JSON data passed to serializer.
         serializer = MailSerializer(data=request.data)
+        # Check if the mail is a valid GUC mail.
         if (serializer.is_valid()):
             return Response(data={"Validity": "Valid GUCmail"}, status=status.HTTP_200_OK)
         return Response(data={"Validity": serializer.errors["mail"][0]}, status=status.HTTP_406_NOT_ACCEPTABLE)
